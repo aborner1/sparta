30a23689f1c17063e7dfc74394d228bc5e749d63
Arnaud Borner <42391399+aborner1@users.noreply.github.com>
2021-07-07 13:42:55 -0700

    Update collide_vss.cpp
    Fix issue with vibrel MW temperature exponent.

--- src/collide_vss.cpp
+++ src/collide_vss.cpp
@@ -1008,8 +1008,8 @@ double CollideVSS::vibrel_serial(int isp, double Ec)
   double omega = params[isp][isp].omega;
   double diam = params[isp][isp].diam;
   double tref = params[isp][isp].tref;
-  double vibphi = 4.0 * MY_PIS * pow(diam,2.0)*pow(tref,omega)
-                  * pow(Tr,-omega-0.5)*101325.0*exp(params[isp][isp].vibc1
+  double vibphi = 4.0 * MY_PIS * pow(diam,2.0)*pow(tref,omega-0.5)
+                  * pow(Tr,-omega)*101325.0*exp(params[isp][isp].vibc1
                   * (pow(Tr,-1.0/3.0)-params[isp][isp].vibc2)-18.42)/sqrt(species[isp].mass*update->boltz);
   return vibphi;
 }
@@ -1036,8 +1036,8 @@ double CollideVSS::vibrel_prohibdouble(int isp, double Ec, double doftot)
 {
   Particle::Species *species = particle->species;
   double Tr = 2.0 * Ec / (update->boltz * doftot);
-  double Zmw = 4.0*MY_PIS * pow(params[isp][isp].diam,2.0)*pow(params[isp][isp].tref,params[isp][isp].omega)
-                  * pow(Tr,-params[isp][isp].omega-0.5)*101325.0*exp(params[isp][isp].vibc1
+  double Zmw = 4.0*MY_PIS * pow(params[isp][isp].diam,2.0)*pow(params[isp][isp].tref,params[isp][isp].omega-0.5)
+                  * pow(Tr,-params[isp][isp].omega)*101325.0*exp(params[isp][isp].vibc1
                   * (pow(Tr,-1.0/3.0)-params[isp][isp].vibc2)-18.42)/sqrt(species[isp].mass*update->boltz);
   double Zpark = 4.0*MY_PI*pow(params[isp][isp].diam,2.0)*pow(params[isp][isp].tref,params[isp][isp].omega-0.5)
                           *pow(Tr,2.5-params[isp][isp].omega)/(2.5e9*params[isp][isp].park);
